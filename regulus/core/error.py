128022022fd9f95459ea1e5b605cb62aa088f4e1
brain-fuel <matt@brain-fuel.co>
2026-06-30 12:02:09 -0400

    fix: restore 'computational expression' docstring (over-broad rename)

--- regulus/core/error.py
+++ regulus/core/error.py
@@ -2,7 +2,7 @@ from typing import NoReturn
 
 
 class EffectError(Exception):
-    """An error that will exit any computational regulus.
+    """An error that will exit any computational expression.
 
     We use this to detect if sub-generators causes an exit, since
     yielding nothing will be silently ignored.
